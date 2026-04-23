ca3386d2d45add1ddb2a484bf3f1bcc5fe48337f
itkhanz <itkhanz@gmail.com>
2026-04-23 21:22:18 +0200

    feat: Post, Put and Delete Requestor

--- tests/conduit-bondaracademy/smokeTest.spec.ts
+++ tests/conduit-bondaracademy/smokeTest.spec.ts
@@ -2,6 +2,17 @@ import { expect } from "@playwright/test";
 import { test } from "../../utils/fixtures.ts";
 import { RequestHandler } from "../../utils/RequestHandler.ts";
 
+let authToken: string
+
+test.beforeAll('Get Token', async ({ api }) => {
+    const tokenResponse = await api
+        .path('/users/login')
+        .body({ "user": { "email": "itkhanz@test.com", "password": "test1234" } })
+        .postRequest(200)
+
+    authToken = 'Token ' + tokenResponse.user.token
+})
+
 //Providing api fixture as input instead of request object
 test('Get Articles', async ({ api }) => {
 
@@ -24,4 +35,45 @@ test('Get Test Tags', async ({ api }) => {
 
     expect(response.tags.length).toBeLessThanOrEqual(10)
     expect(response.tags[0]).toEqual('Test')
+})
+
+test('Create, Update, Delete, and Get Article', async ({ api }) => {
+
+    const newArticleResponse = await api
+        .path('/articles')
+        .headers({ Authorization: authToken })
+        .body({ "article": { "title": "Second article", "description": "About", "body": "Details", "tagList": ["test"] } })
+        .postRequest(201)
+
+    expect(newArticleResponse.article.title).toEqual("Second article")
+    const articleSlug = newArticleResponse.article.slug
+
+    const updateArticleResponse = await api
+        .path(`/articles/${articleSlug}`)
+        .headers({ Authorization: authToken })
+        .body({ "article": { "title": "Edited article", "description": "About", "body": "Details", "tagList": ["test"] } })
+        .putRequest(200)
+
+    expect(updateArticleResponse.article.title).toEqual("Edited article")
+    const updatedArticleSlug = updateArticleResponse.article.slug
+
+    const getArticlesResponse = await api
+        .path('/articles')
+        .headers({ Authorization: authToken })
+        .params({ limit: 10, offset: 0 })
+        .getRequest(200)
+    expect(getArticlesResponse.articles[0].title).toEqual('Edited article')
+
+    const deleteArticleResponse = await api
+        .path(`/articles/${updatedArticleSlug}`)
+        .headers({ Authorization: authToken })
+        .deleteRequest(204)
+
+    const finalArticlesResponse = await api
+        .path('/articles')
+        .headers({ Authorization: authToken })
+        .params({ limit: 10, offset: 0 })
+        .getRequest(200)
+    expect(finalArticlesResponse.articles[0].title).not.toEqual('Edited article')
+
 })
